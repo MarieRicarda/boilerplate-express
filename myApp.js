7eeac1f225e27bec4437b0eca48d661e46509502
MarieRicarda <handcraftedbymarie@gmail.com>
2022-04-07 10:12:30 -0700

    added dotenv package & connected it to myApp

--- myApp.js
+++ myApp.js
@@ -1,16 +1,31 @@
 var express = require('express');
 var app = express();
+var bGround = require('fcc-express-bground');
+require('dotenv').config();
 
 
 console.log("Hello World");
 
+/*
 app.get("/json", function(req, res) {
     res.json(
         {"message": "Hello json"}
     );
 });
+*/
 
-
+app.get("/json", function(req, res) {
+    console.log(process.env.MESSAGE_STYLE, "<=message style");
+    if (process.env.MESSAGE_STYLE === "uppercase") {
+        res.json(
+            { "message": "HELLO JSON" }
+        )
+    } else {
+        res.json(
+            { "message": "Hello json" }
+        )
+    }
+});
 
 
 
