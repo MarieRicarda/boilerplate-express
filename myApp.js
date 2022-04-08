ae2166aea27b1a951e2561cc1fee4bc2e4d5b5e7
MarieRicarda <handcraftedbymarie@gmail.com>
2022-04-08 10:13:14 -0700

    modified myApp.js to show the local time

--- myApp.js
+++ myApp.js
@@ -32,6 +32,17 @@ app.get("/json", function(req, res) {
     }
 });
 
+function getTheCurrentTimeString() {
+    return new Date().toString();
+}
+
+app.get("/now", function(req, res, next) {
+    req.time = getTheCurrentTimeString();
+    next();
+}, function(req, res) {
+    res.json({ time: req.time });
+})
+
 
 
 
